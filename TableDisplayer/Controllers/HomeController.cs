287393bbf92ad737dbb30d3f5c5cddf87ec08594
vitalii.lozovitsky <vitalii.lozovitsky@digitallyinspired.com>
2021-08-10 19:53:13 +0300

    Added verification file

--- TableDisplayer/Controllers/HomeController.cs
+++ TableDisplayer/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using TableDisplayer.Models;
@@ -23,6 +24,13 @@ namespace TableDisplayer.Controllers {
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        [Route("~/.well-known/pki-validation/5ECE9808BF4D0D668F9659B5D591F467.txt")]
+        public VirtualFileResult GetVerificationFile() {
+            var path = Path.Combine("~/files", "5ECE9808BF4D0D668F9659B5D591F467.txt");
+            return File(path, "text/plain", "5ECE9808BF4D0D668F9659B5D591F467.txt");
         }
     }
 }
